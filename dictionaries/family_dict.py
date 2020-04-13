415f8606ea436a2a9522246de9758e2764494386
Tyler Simeone <tyler.simeone@gmail.com>
2020-04-13 13:04:11 -0500

    working on family dictionary challenge

---
+++ dictionaries/family_dict.py
@@ -0,0 +1,19 @@
+my_family = {
+    "me": {
+        "name": "Tyler",
+        "age": 24
+    },
+    "dad": {
+        "name": "Chris",
+        "age": 59
+    },
+    "mom": {
+        "name": "Lisa",
+        "age": 57
+    }
+}
+
+# print(my_family.items())
+
+# family_string = [f"{value["name"]} is my {key} and is {str(value["age"])} years old." for key, value in my_family.items()]
+# print(family_string)
