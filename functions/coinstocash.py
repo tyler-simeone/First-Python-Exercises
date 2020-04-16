59a167e02af3c92745342d25f96f62219a3f5885
Tyler Simeone <tyler.simeone@gmail.com>
2020-04-16 10:47:35 -0500

    coins to cash funcs practice complete

--- functions/coinstocash.py
+++ functions/coinstocash.py
@@ -1,16 +1,23 @@
 def calc_dollars(**coins):
     coins = coins.items()
-    print(coins)
+
+    total = 0
 
     # The function should look at each key (pennies, nickels, dimes and quarters) and perform the appropriate math on the integer value to determine how much money you have in dollars. Store that value in a variable named `dollarAmount` and print it.
     for key, value in coins:
         if key == "pennies":
-            print(f"{key}: {value}")
+            dollar_amt = value / 100
+            total += dollar_amt
         elif key == "nickels":
-            print(f"{key}: {value}")
+            dollar_amt = value / 20
+            total += dollar_amt
         elif key == "dimes":
-            print(f"{key}: {value}")
+            dollar_amt = value / 10
+            total += dollar_amt
         elif key == "quarters":
-            print(f"{key}: {value}")
+            dollar_amt = value / 4
+            total += dollar_amt
+
+    print(total)
 
 calc_dollars(pennies= 342, nickels=9, dimes=32, quarters=4)
